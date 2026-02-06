867d4eba55653fa6c113cc1ecf248397651f531f
Isaac Yeh <101854219+IsaacYeh2009@users.noreply.github.com>
2026-02-06 15:59:11 -0600

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,20 +1,5 @@
-/*const button = document.getElementById("clickBtn");
-const counter = document.getElementById("counter");
+alert("script.js loaded");
 
-const protocol = location.protocol === "https:" ? "wss" : "ws";
-const socket = new WebSocket(`${protocol}://${location.host}`);
-
-socket.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-  counter.textContent = data.count;
-};
-
-button.addEventListener("click", () => {
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send("click");
-  }
-});
-*/
 const button = document.getElementById("clickBtn");
 const counter = document.getElementById("counter");
 
@@ -27,22 +12,11 @@ socket.onopen = () => {
   console.log("WebSocket connected");
 };
 
-socket.onerror = (err) => {
-  console.error("WebSocket error:", err);
-};
-
-socket.onmessage = (event) => {
-  console.log("Message from server:", event.data);
-  const data = JSON.parse(event.data);
-  counter.textContent = data.count;
-};
-
 button.addEventListener("click", () => {
-  console.log("Button clicked, socket state:", socket.readyState);
+  alert("Button click detected in JS");
+
   if (socket.readyState === WebSocket.OPEN) {
     socket.send("click");
-    console.log("Click sent to server");
-  } else {
-    console.warn("WebSocket not open");
+    console.log("Click sent");
   }
 });
